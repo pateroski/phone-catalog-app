11616aa0bac6d16de80e659f2aa7232e15719d3a
Andrés Reyes <p4ter0ski@gmail.com>
2019-10-03 17:08:56 +0200

    [styles] add styles to Pagination component

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import classes from './Pagination.module.css';
 
-const pagination = props => (
-  <ul className={classes.Pagination}>
-    <li className={classes.PageItem}>
-      <button onClick={props.clicked.bind(this, 1)}>1</button>
-    </li>
-    <li>
-      <button onClick={props.clicked.bind(this, 2)}>2</button>
-    </li>
-  </ul>
-);
+const pagination = props => {
+  return (
+    <ul className={classes.Pagination}>
+      <li className={classes.PageItem}>
+        <button
+          className={classes.NavigateBtn}
+          onClick={props.clicked.bind(this, 1)}>
+          1
+        </button>
+      </li>
+      <li className={classes.PageItem}>
+        <button
+          className={classes.NavigateBtn}
+          onClick={props.clicked.bind(this, 2)}>
+          2
+        </button>
+      </li>
+    </ul>
+  );
+};
 
 export default pagination;
