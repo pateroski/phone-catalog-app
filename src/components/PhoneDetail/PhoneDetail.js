dba107ae9445925cde0efde1dca4e7a41f40df20
Andrés Reyes <p4ter0ski@gmail.com>
2019-10-03 16:30:59 +0200

    [feat] add HTML structure and CSS for PhoneDetail

--- src/components/PhoneDetail/PhoneDetail.js
+++ src/components/PhoneDetail/PhoneDetail.js
@@ -1,19 +1,60 @@
 import React from 'react';
 import classes from './PhoneDetail.module.css';
 
-const phoneDetail = props => (
-  <div className={classes.PhoneCard}>
-    <img src={props.phone.image} alt={props.phone.name} />
-    <div className={classes.Description}>
-      <span className={classes.Name}>{props.phone.name}</span>
-      <span className={classes.Price}>{props.phone.price}</span>
-    </div>
-    <button
-      className={classes.ShowExitBtn}
-      onClick={props.clicked.bind(this, false)}>
-      Salir
-    </button>
-  </div>
-);
+const phoneDetail = props => {
+  const colors =
+    Object.keys(props.phone).length > 0 ? props.phone.colors.join(', ') : null;
+  return (
+    <article className={classes.PhoneDetail}>
+      <section className={classes.PhoneDescription}>
+        <img
+          className={classes.PhoneImage}
+          src={props.phone.image}
+          alt={props.phone.name}
+        />
+        <ul className={classes.DescriptionList}>
+          <li>
+            <span className={classes.DescriptionItem}>Brand: </span>
+            <span>{props.phone.brand}</span>
+          </li>
+          <li>
+            <span className={classes.DescriptionItem}>Model: </span>
+            <span>{props.phone.name}</span>
+          </li>
+          <li>
+            <span className={classes.DescriptionItem}>Price: </span>
+            <span>{props.phone.price}</span>
+          </li>
+          <li>
+            <span className={classes.DescriptionItem}>Year: </span>
+            <span>{props.phone.year}</span>
+          </li>
+          <li>
+            <span className={classes.DescriptionItem}>Colors: </span>
+            <span>{colors}</span>
+          </li>
+          <li>
+            <span className={classes.DescriptionItem}>Description: </span>
+            <span>{props.phone.description}</span>
+          </li>
+          <li>
+            <a
+              href={props.phone.web}
+              className={classes.VisitWebLink}
+              target="_blank"
+              rel="noopener noreferrer">
+              Visita la web
+            </a>
+          </li>
+        </ul>
+      </section>
+      <button
+        className={classes.ShowExitBtn}
+        onClick={props.clicked.bind(this, false)}>
+        Salir
+      </button>
+    </article>
+  );
+};
 
 export default phoneDetail;
